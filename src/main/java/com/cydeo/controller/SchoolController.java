739a1643f8bd0fab7b0adf963841476ee290f502
jamaldemir <jamal@cybertekschool.com>
2022-12-17 12:35:39 -0500

    Address get and put is completed

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,14 +1,15 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.AddressDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.AddressService;
 import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,11 +19,13 @@ public class SchoolController {
     private final TeacherService teacherService;
     private final StudentService studentService;
     private final ParentService parentService;
+    private final AddressService addressService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService, AddressService addressService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
         this.parentService = parentService;
+        this.addressService = addressService;
     }
     //write a method/endpoint for teachers and return as a list of teachers
 
@@ -69,6 +72,40 @@ public class SchoolController {
                             .body(responseWrapper);
 
         }
+
+        /*
+            create an endpoint for individual address information
+            /address/1
+            return status code 200
+            "address .. is successfully retrieved" message
+            and address information
+            5MIN
+         */
+    @GetMapping("/address/{id}")
+    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable("id") Long id) throws Exception {
+        AddressDTO addressToReturn = addressService.findById(id);
+
+        return ResponseEntity.ok(new ResponseWrapper("Address "+id+" is successfully retrieved",addressToReturn));
+
+    }
+
+
+    /*
+        create an endpoint to update individual address information.
+        return updated address directly.
+        5MIN
+     */
+    @PutMapping("/address/{id}")
+    public AddressDTO updateAddress(@PathVariable("id") Long id, @RequestBody AddressDTO addressDTO) throws Exception {
+
+        addressDTO.setId(id);
+
+        AddressDTO updateAddress = addressService.update(addressDTO);
+
+        return updateAddress;
+    }
+
+
 
 
 }

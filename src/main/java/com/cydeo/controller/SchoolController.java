ae5d7959665543d82f746c8875d3206bb8bf0ea9
civanozbay <civan417@hotmail.com>
2022-12-20 22:15:13 +0100

    properties and teachers api added

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,111 +1,24 @@
 package com.cydeo.controller;
 
-import com.cydeo.dto.AddressDTO;
-import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
-import com.cydeo.service.AddressService;
-import com.cydeo.service.ParentService;
-import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1")
 public class SchoolController {
-
     private final TeacherService teacherService;
-    private final StudentService studentService;
-    private final ParentService parentService;
-    private final AddressService addressService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService, AddressService addressService) {
+    public SchoolController(TeacherService teacherService) {
         this.teacherService = teacherService;
-        this.studentService = studentService;
-        this.parentService = parentService;
-        this.addressService = addressService;
     }
-    //write a method/endpoint for teachers and return as a list of teachers
 
     @GetMapping("/teachers")
-    public List<TeacherDTO> allTeachers(){
-        List<TeacherDTO> teacherDTOS = teacherService.findAll();
-
-        return teacherDTOS;
+    public List<TeacherDTO> getTeachers(){
+        return teacherService.findAll();
     }
-
-    /*
-        create a endpoint for students, where json includes
-        "Students are successfully retrieved." message
-        code: 200
-        success:true
-        and student data
-        8MIN
-     */
-
-    @GetMapping("/students")
-    public ResponseEntity<ResponseWrapper> readAllStudents(){
-        return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved",studentService.findAll()));
-    }
-
-    /*
-        create a parents endpoint where status code is 202
-        additional header has "Parent","Returned"
-        and following json body structure
-        "Parents are successfully retrieved." message
-        code: 200
-        success:true
-        and parents data
-        7MIN
-     */
-        @GetMapping("/parents")
-        public ResponseEntity<ResponseWrapper> readAllParents(){
-            ResponseWrapper responseWrapper = new ResponseWrapper(true,
-                    "Parents are successfuly retrieved",
-                    HttpStatus.OK.value(), parentService.findAll());
-
-            return ResponseEntity
-                            .status(HttpStatus.ACCEPTED)
-                            .header("Parent","Returned")
-                            .body(responseWrapper);
-
-        }
-
-        /*
-            create an endpoint for individual address information
-            /address/1
-            return status code 200
-            "address .. is successfully retrieved" message
-            and address information
-            5MIN
-         */
-    @GetMapping("/address/{id}")
-    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable("id") Long id) throws Exception {
-        AddressDTO addressToReturn = addressService.findById(id);
-
-        return ResponseEntity.ok(new ResponseWrapper("Address "+id+" is successfully retrieved",addressToReturn));
-
-    }
-
-
-    /*
-        create an endpoint to update individual address information.
-        return updated address directly.
-        5MIN
-     */
-    @PutMapping("/address/{id}")
-    public AddressDTO updateAddress(@PathVariable("id") Long id, @RequestBody AddressDTO addressDTO) throws Exception {
-
-        addressDTO.setId(id);
-
-        AddressDTO updateAddress = addressService.update(addressDTO);
-
-        return updateAddress;
-    }
-
-
-
-
 }
